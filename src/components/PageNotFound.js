9a6e961679a3620331be1232fc41eaed87b4f405
leo6liu <leo.battalora@gmail.com>
2022-01-12 23:20:17 -0500

    404 page

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,7 @@
+export default function PageNotFound() {
+  return (
+    <div className="flex flex-col justify-center items-center h-80 text-xl font-mono">
+      404 (page not found)
+    </div>
+  );
+}
