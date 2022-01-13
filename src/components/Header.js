93fc51f46adbf52a81a1470762e6078b0099d238
leo6liu <leo.battalora@gmail.com>
2022-01-13 00:05:40 -0500

    added alt text

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     <div className="flex justify-center py-4">
       <Link to="/">
         <div className="flex items-center gap-4 text-4xl font-semibold">
-          <img src={bible} className="w-16 h-16"></img>
+          <img src={bible} alt="bible" className="w-16 h-16"></img>
           <h1>BibleDev</h1>
         </div>
       </Link>
