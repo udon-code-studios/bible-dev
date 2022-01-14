7fae0173f1866bdff528531f17ec298c4c76bf78
leo6liu <leo.battalora@gmail.com>
2022-01-13 22:56:29 -0500

    moved header out of default layout and added book endpoint to router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import * as React from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
+import Book from './pages/Book'
 import Footer from './components/Footer';
-import Header from './components/Header';
 import Home from './pages/Home';
 import PageNotFound from './pages/PageNotFound';
-import Testament from './pages/Testament';
 
 export default function App() {
   const location = useLocation();
 
   return (
     <div className="relative flex flex-col min-h-screen bg-stone-400 text-stone-900">
-      <Header />
-
+      
       <AnimatePresence exitBeforeEnter>
         <Routes location={location} key={location.pathname}>
           <Route path="/">
             <Route index element={<Home />} />
-            <Route path="ot" element={<Testament testament="ot" />} />
-            <Route path="nt" element={<Testament testament="nt" />} />
+            <Route path=":book" element={<Book />} />
             <Route path="*" element={<PageNotFound />} />
           </Route>
         </Routes>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import Book from '../components/Book'
+import Header from '../components/Header';
 
 export default function Home() {
   const variants = {
@@ -12,15 +12,19 @@ export default function Home() {
     <motion.div
       initial="hidden" animate="visible"
       variants={variants} transition={{ duration: 1 }}
-      className="flex flex-row justify-center items-center py-16 px-8 gap-6">
-      <div>
-        <h1 className="text-3xl font-serif font-bold">The Bible</h1>
-        <p className="pr-3 max-w-md">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+    >
+      <Header />
+      <div
+        className="flex flex-row justify-center items-center py-16 px-8 gap-6">
+        <div>
+          <h1 className="text-3xl font-serif font-bold">The Bible</h1>
+          <p className="pr-3 max-w-md">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div className="text-stone-400"><Book width="14rem" title="Old Testament" /></div>
+        <div className="text-stone-400"><Book width="10rem" title="New Testament" /></div>
       </div>
-      <Link to="/ot" className="text-stone-400"><Book width="14rem" title="Old Testament" /></Link>
-      <Link to="/nt" className="text-stone-400"><Book width="10rem" title="New Testament" /></Link>
     </motion.div>
   );
 }
