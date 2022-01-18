bea9d805086897d9aa5e1373518055f988239916
leo6liu <leo.battalora@gmail.com>
2022-01-18 13:42:23 -0500

    added footer to all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Footer from '/components/Footer';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="relative flex flex-col min-h-screen bg-stone-400 text-stone-900">
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default MyApp
