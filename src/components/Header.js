bf47bcc94ecb9f3c5e3fe9c9d65abdb62d7cf552
leo6liu <leo.battalora@gmail.com>
2022-01-17 21:34:39 -0500

    changed header to grid layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,13 @@ import bible from '../assets/bible.png'
 
 export default function Header() {
   return (
-    <div className="flex justify-center py-4">
+    <div className="grid grid-flow-col justify-center py-4">
       <Link to="/">
         <div className="flex items-center gap-4 text-4xl font-semibold">
           <img src={bible} alt="bible" className="w-16 h-16"></img>
           <h1>BibleDev</h1>
         </div>
       </Link>
-
     </div>
   );
 }
