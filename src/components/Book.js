2363093d43a255a2dbe6bfac8231b15adf37cc47
leo6liu <leo.battalora@gmail.com>
2022-01-17 21:34:23 -0500

    removed book shadow

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,7 @@
 export default function Book({ width, title }) {
   return (
     <div
-      className="flex shrink-0 justify-center items-center min-w-[2rem] h-80 rounded-3xl bg-stone-800 text-inherit text-2xl text-center font-serif hover:shadow-2xl"
+      className="flex shrink-0 justify-center items-center min-w-[2rem] h-80 rounded-3xl bg-stone-800 text-inherit text-2xl text-center font-serif"
       style={{ width: width }}
     >
       <div className="rotate-90 whitespace-nowrap">{title}</div>
