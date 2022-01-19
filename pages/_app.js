cf15f29a2d72ce03aad88366cf29fc95eaa0195b
Leo <leo.battalora@gmail.com>
2022-01-19 17:17:38 -0500

    removed empty <> </>

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,10 @@ import Footer from '/components/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <div className="relative flex flex-col min-h-screen bg-stone-400 text-stone-900">
-        <Component {...pageProps} />
-        <Footer />
-      </div>
-    </>
+    <div className="relative flex flex-col min-h-screen bg-stone-400 text-stone-900">
+      <Component {...pageProps} />
+      <Footer />
+    </div>
   );
 }
 
