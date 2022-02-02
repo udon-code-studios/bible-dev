336f5de3415e650a25cc42d3557e24878ee0399c
Joshua Liu <74061612+Genius6942@users.noreply.github.com>
2022-02-02 08:17:41 -0500

    Delete makedata.js

--- makedata.js
+++
@@ -1,9 +0,0 @@
-function item(min, max, length) {
-  const start = Math.round(Math.random() * (max - min) + min);
-  return {
-    date: start,
-    name: 'book ' + start.toString(),
-  }
-}
-
-console.log(new Array(10).fill().map(x => item(-2000, 1000, 100)));
