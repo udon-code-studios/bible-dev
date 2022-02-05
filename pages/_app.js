71fe74e1db0ed2aa031d0208b0bd068fa7014ea5
leo6liu <leo.battalora@gmail.com>
2022-02-05 14:42:11 -0500

    correct growth for book screen

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,9 @@ import Footer from '/components/Footer';
 export default function App({ Component, pageProps }) {
   return (
     <div className="relative flex flex-col justify-between min-h-screen bg-stone-400 text-stone-900">
-      <Component {...pageProps} />
+      <div className="flex grow">
+        <Component {...pageProps} />
+      </div>
       <Footer />
     </div>
   );
