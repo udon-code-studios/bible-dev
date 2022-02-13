efed2e53d097d87e9a79502fa0a7c478a8fbc000
Genius6942 <supajoshjunk@gmail.com>
2022-02-13 18:10:58 -0500

    mostly done with search - needs click to exit search

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
 // file: pages/index.js
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { motion } from 'framer-motion';
 import Book from '/components/Book';
+import { getCollection } from '../lib/mongodb';
 
 // TODO('add description')
-export default function Page() {
+export default function Page({ books }) {
   const [view, setView] = useState('home'); // states: 'home', 'old', 'new'
   const router = useRouter(); // used to change routing programmatically
 
@@ -63,6 +64,47 @@ export default function Page() {
     );
   }
 
+	// Hooks for search bar
+	const searchRef = useRef(null);
+
+	/**
+	 * @param {HTMLElement} child 
+	 * @param {HTMLElement} parent 
+	 */
+	const isDeepChildOf = (child, parent) => {
+		if (child === parent) {
+			return true;
+		} else if (child === document.body || document.documentElement || document.head) {
+			return false;
+		} else {
+			return isDeepChildOf(child.parentNode);
+		}
+	}
+
+	useEffect(() => {
+		window.addEventListener('keydown', (e) => {
+			if (e.key === '/' || (e.key === 'k' && e.ctrlKey)) {
+				if (searchRef.current && e.target !== searchRef.current) {
+					e.preventDefault();
+					searchRef.current.focus();
+				}
+			} else if (e.key === 'Escape') {
+				searchRef.current.value = '';
+				searchRef.current.blur();
+				setSearchItems('hidden');
+				setSelected(null);
+				setInputValue('');
+			}
+		});
+	}, []);
+
+	// list of books to be shown under search
+	const [searchItems, setSearchItems] = useState('hidden');
+	// selected book in list
+	const [selected, setSelected] = useState(null);
+	// current value of input
+	const [inputValue, setInputValue] = useState('');
+
   return (
     <>
       <Head>
@@ -73,7 +115,7 @@ export default function Page() {
 
       <main>
         {/* header */}
-        <div className="grid grid-flow-col justify-center py-4">
+        <div className="grid grid-flow-col justify-center py-4 z-10">
           <Link href="/">
             <a>
               <div className="flex items-center gap-4 text-4xl font-semibold">
@@ -84,6 +126,82 @@ export default function Page() {
               </div>
             </a>
           </Link>
+					<div className="relative my-auto h-6 w-48">
+						<div
+							className='absolute max-h-[75vh] ml-3 bg-white overflow-y-auto nobar rounded-xl z-20 shadow-2xl'
+							onKeyDown={
+								(e) => {
+									if (searchItems instanceof Array && searchItems.length > 0) {
+										if (e.key === 'ArrowUp') {
+											e.preventDefault();
+											const idx = (typeof selected !== 'number') ? searchItems.length - 1 : (selected > 0) ? selected - 1 : searchItems.length - 1;
+											setSelected(idx);
+											searchRef.current.value = searchItems[idx].name;
+										} else if (e.key === 'ArrowDown') {
+											e.preventDefault();
+											const idx = (typeof selected !== 'number') ? 0 : (selected < searchItems.length - 1) ? selected + 1 : 0;
+											setSelected(idx);
+											searchRef.current.value = searchItems[idx].name;
+										} else if (e.key === 'Enter' && searchItems[selected]) {
+											router.push(`/${searchItems[selected].testament}/${searchItems[selected].name.toLowerCase()}`);
+										}
+									}
+								}
+							}
+						>
+							<input
+								onChange={
+									(e) => {
+										const v = e.target.value.toLowerCase();
+										setInputValue(v);
+										if (v.length >= 1) {
+											let searchedBooks = books.filter(book => book.name.toLowerCase().includes(v.toLowerCase()));
+											searchedBooks = searchedBooks.sort((first, second) => first.name.toLowerCase().indexOf(v) - second.name.toLowerCase().indexOf(v));
+											searchedBooks.length = Math.min(searchedBooks.length, Infinity);
+											setSearchItems(searchedBooks);
+										} else {
+											setSearchItems('hidden');
+										}
+										setSelected(null);
+									}
+								}
+								ref={ searchRef }
+								placeholder="Press '/' to search"
+								className="w-48 h-6 p-3 border-2 rounded-xl focus-visible:outline-none focus-visible:border-black"
+							/>
+							<div
+								className="w-full bg-white rounded-xl"
+								style={{ display: searchItems === 'hidden' ? 'hidden' : 'block'}}
+							>
+								{ (() => {
+									if (searchItems === 'hidden') {
+										return null;
+									} else if (searchItems.length > 0) {
+										return (
+											<div className='relative'>
+												{ searchItems.map((item, idx) => (
+												<div key={idx}>
+													<Link href={`/${item.testament}/${item.name.toLowerCase()}`} className="w-full">
+														<a
+															className='block w-full text-center rounded-xl capitalize pl-2'
+															style={{ backgroundColor: (idx === selected) ? 'rgba(0, 0, 0, .2)' : 'transparent' }}
+														>
+															{ item.name.substring(0, item.name.toLowerCase().indexOf(inputValue)) }
+															<strong>{ inputValue }</strong>
+															{ item.name.substring(item.name.toLowerCase().indexOf(inputValue) + inputValue.length) }
+														</a>
+													</Link>
+												</div>
+											))}
+											</div>
+										);
+									} else {
+										return (searchItems.length > 0) ? searchItems : 'No Results Found';
+									}
+								})() }
+							</div>
+						</div>
+					</div>
         </div>
 
         <motion.div
@@ -139,6 +257,17 @@ export default function Page() {
       </main>
     </>
   )
+}
+
+export async function getStaticProps() {
+	return {
+		props: {
+			books: (await getCollection('books')).map(book => ({
+				name: book.name,
+				testament: book.testament,
+			})),
+		}
+	};
 }
 
 //
