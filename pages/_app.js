d3fe776425c256e47e837a19138df4207ffcdc2e
leo6liu <leo.battalora@gmail.com>
2022-02-19 20:02:17 -0500

    moved footer from _app to index pages

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,7 @@ import Footer from '/components/Footer';
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="relative flex flex-col justify-between min-h-screen bg-stone-400 text-stone-900">
-      <Component {...pageProps} />
-      <Footer />
-    </div>
+    <Component {...pageProps} />
   );
 }
 
