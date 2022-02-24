72855325934349b5f672d858175bcc8df337e99e
Genius6942 <supajoshjunk@gmail.com>
2022-02-24 17:25:42 -0500

    Markdown spaces insted of tabs

--- components/Markdown.js
+++ components/Markdown.js
@@ -2,27 +2,27 @@ import ReactMarkdown from 'react-markdown';
 import Link from 'next/link';
 
 export default function Markdown({ text, children }) {
-	return (
-		<ReactMarkdown
-			components={{
-				// bypass some default tailwind stuff
-				h1: ({ node, ...props }) => <span className="text-6xl" {...props}></span>,
-				h2: ({ node, ...props }) => <span className="text-5xl" {...props}></span>,
-				h3: ({ node, ...props }) => <span className="text-4xl" {...props}></span>,
-				h4: ({ node, ...props }) => <span className="text-3xl" {...props}></span>,
-				h5: ({ node, ...props }) => <span className="text-2xl" {...props}></span>,
-				h6: ({ node, ...props }) => <span className="text-xl" {...props}></span>,
-				// use next Link instead of <a>
-				a: ({ node, ...props }) => (
-					<Link href = { props.href }>
-						<a className="underline hover:text-blue-600">
-							{props.children}
-						</a>
-					</Link>
-				)
-			}}
-		>
-			{ children ? children : text }
-		</ReactMarkdown>
-	)
+  return (
+    <ReactMarkdown
+      components={{
+        // bypass some default tailwind stuff
+        h1: ({ node, ...props }) => <span className="text-6xl" {...props}></span>,
+        h2: ({ node, ...props }) => <span className="text-5xl" {...props}></span>,
+        h3: ({ node, ...props }) => <span className="text-4xl" {...props}></span>,
+        h4: ({ node, ...props }) => <span className="text-3xl" {...props}></span>,
+        h5: ({ node, ...props }) => <span className="text-2xl" {...props}></span>,
+        h6: ({ node, ...props }) => <span className="text-xl" {...props}></span>,
+        // use next Link instead of <a>
+        a: ({ node, ...props }) => (
+          <Link href = { props.href }>
+            <a className="underline hover:text-blue-600">
+              {props.children}
+            </a>
+          </Link>
+        )
+      }}
+    >
+      { children ? children : text }
+    </ReactMarkdown>
+  )
 }
